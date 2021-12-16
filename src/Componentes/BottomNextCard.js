aa38a20440a40c4a2faa3656a693696b3fbf624f
Eduardo Vedooto <vedootoeduardo@gmail.com>
2021-12-16 10:24:05 -0300

    Preparando revisão de código

--- src/Componentes/BottomNextCard.js
+++
@@ -1,7 +0,0 @@
-export default function BottomNextCard({nextCard,}){
-    return(
-        <div className="bottom-card" data-identifier="arrow">
-            <img onClick={nextCard} src="./assets/turn.png" alt="seta de giro"/>
-        </div>
-    );
-}
